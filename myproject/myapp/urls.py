7ea3fd81f1e39b9ddc2c1b08feec8d6528ebaee5
Ankit-8126 <[ankitparmar8126@gmail.com]>
2026-04-14 13:14:45 +0530

    first commit

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home ),
+    path('about/',views.about),
+    path('features/',views.features),
+    path('contact/',views.contact),
+    path('login/',views.login)
+    
+    
+]
